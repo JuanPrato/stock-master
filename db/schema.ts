76847e312591a5d0522b0836bdccb8c80bafa985
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-08-26 22:36:50 -0300

    advance db integration

---
+++ db/schema.ts
@@ -0,0 +1,13 @@
+import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const products = sqliteTable("products", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  description: text("description"),
+  price: real("price").notNull().default(0),
+  stock: integer("stock").notNull().default(0),
+  creationDate: integer("creation_date", { mode: "timestamp" }).default(
+    new Date()
+  ),
+  modifyDate: integer("modify_date", { mode: "timestamp" }).default(new Date()),
+});
---
+++ src/app/api/product/route.ts
@@ -0,0 +1,10 @@
+import { db } from "@/lib/db";
+import { products as productsTable } from "../../../../db/schema";
+
+export async function GET() {
+  const products = await db.select().from(productsTable);
+
+  return Response.json({
+    products: products,
+  });
+}
