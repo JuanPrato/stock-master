662130646b8f3d3d85db750dec6754f1aa1bc6ad
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-09-01 13:02:21 -0300

    parse errors utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function parseErrors(error: z.ZodError<FormData>) {
+  return error.errors.reduce((acc, err) => {
+    acc[err.path[0]] = err.message;
+    return acc;
+  }, {} as any);
 }
