dd2ad6cd34a92f0b22f5802939b2d7a4eccf1d5e
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-09-08 11:29:44 -0300

    use env to prd

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import { env } from "process";
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
-    url: "file:./sqlite.db",
+    url: env.DB_URL!,
+    authToken: env.DB_AUTH_TOKEN!,
   },
 });
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,13 @@ import { createClient } from "@libsql/client";
 import { unstable_cache } from "next/cache";
 import { category } from "@/db/schema";
 
-const client = createClient({ url: "file:./sqlite.db" });
+const url = process.env.DB_URL;
+
+if (!url) {
+  throw new Error();
+}
+
+const client = createClient({ url, authToken: process.env.DB_AUTH_TOKEN });
 
 export const db = drizzle(client);
 
