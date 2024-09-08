2d7cacf4007e24af65c133213f970bd150e3b5ed
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-09-08 12:44:45 -0300

    try host URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
