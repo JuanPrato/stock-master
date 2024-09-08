2f3224ea8d2eb47a7001389d18e39bbb9d5199b2
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-09-08 12:20:05 -0300

    try migrate command

--- src/lib/migrate.ts
+++ src/lib/migrate.ts
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import { db, client } from './db';
 import { migrate } from "drizzle-orm/libsql/migrator";
 
 // This will run migrations on the database, skipping the ones already applied
-await migrate(db, { migrationsFolder: './drizzle' });
+migrate(db, { migrationsFolder: './drizzle' }).catch(console.error);
 
 // Don't forget to close the connection, otherwise the script will hang
 client.close();
